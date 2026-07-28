54dc3fb3b788a420c8d739fa095867733d262b82
tensortea <121320721+tensortea@users.noreply.github.com>
2026-07-28 18:53:29 +0300

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -3,7 +3,7 @@ def add(a,b):
 def substract(a,b):
     return a-b
 def divide(a,b):
-    if a == 0 or b == 0:
+    if b == 0:
         return "На ноль нельзя делить!"
     return a / b
 def multiply(a,b):
@@ -14,6 +14,7 @@ while True:
     ans = int(input("1. Сложить\n2.Отнять\n3.Умножить\n4.Разделить\n0.Выход\n\nВыберите пункт(0-4): "))
     if ans == 0:
         print("До свидания!")
+        break
     elif ans != 1 and ans != 2 and ans != 3 and ans != 4:
         print("Я Вас не понял, повторите, пожалуйста, еще раз...")
     else:
@@ -21,9 +22,9 @@ while True:
         b = float(input("Введите второе число: "))
         if ans == 1:
             print("Ответ:",add(a,b))
-        if ans == 2:
+        elif ans == 2:
             print("Ответ:",substract(a,b))
-        if ans == 3:
+        elif ans == 3:
             print("Ответ:",multiply(a,b))
-        if ans == 4:
+        elif ans == 4:
             print("Ответ:",divide(a,b))
